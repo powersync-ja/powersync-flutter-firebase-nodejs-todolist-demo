e8959158f74ea97c7c4462235caef1a7b66de1af
Mugi Khan <mugikhan@gmail.com>
2024-11-18 19:47:07 +0200

    Fix dart formatting

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,5 +1,6 @@
 /// Update these values
 class AppConfig {
   static const String backendUrl = 'https://4be6-71-211-245-221.ngrok-free.app';
-  static const String powersyncUrl = 'https://65663910ce6b81ac131b8c62.powersync.journeyapps.com';
+  static const String powersyncUrl =
+      'https://65663910ce6b81ac131b8c62.powersync.journeyapps.com';
 }
