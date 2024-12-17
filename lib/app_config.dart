e55a8af1dadaa238756dd0b6779b17d159384840
DominicGBauer <dominic.g.bauer@gmail.com>
2024-12-17 09:35:45 +0200

    chore: remove files and update readme

--- lib/app_config.dart
+++
@@ -1,6 +0,0 @@
-/// Update these values
-class AppConfig {
-  static const String backendUrl = 'https://4be6-71-211-245-221.ngrok-free.app';
-  static const String powersyncUrl =
-      'https://65663910ce6b81ac131b8c62.powersync.journeyapps.com';
-}
